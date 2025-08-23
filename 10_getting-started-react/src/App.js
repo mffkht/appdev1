6fc62841dd57f2c8b7cdd49eab8d6f764f5fa1bf
maffia <maffiakhateg@gmail.com>
2025-08-23 11:33:04 +0800

    Activity 10: Add React About Me project

--- 10_getting-started-react/src/App.js
+++ 10_getting-started-react/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import profile from './img/profile.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn grhhhReact
-        </a>
-      </header>
+      <img className="profile-picture" src={profile} alt="picture"/>
+      <h1 className="full-name">Name: Maffia Khate Guaro</h1>
+      <h1 className="course">Course @ Year: BSIS 3RD Year</h1>
+      <h1 className="fun-fact">Fun Fact: I Can Sleep All Day</h1>
+
+      <h1 className="react">Why I Want to Learn React</h1>
+      <p className="content">
+        I want to learn React because it is one of the most popular tools for building modern websites and applications. Many developers and companies use it to make web pages faster, easier to use, and more interactive. I hope to create projects that are user-friendly and engaging while also improving my skills in web development. I believe this will help me gain more confidence in coding, prepare me for future opportunities in the tech industry, and guide me in becoming a better software professional.
+      </p>
     </div>
   );
 }
