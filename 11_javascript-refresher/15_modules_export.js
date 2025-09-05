234d300265556649993cecc64c161c141d1c01b9
maffia <maffiakhateg@gmail.com>
2025-09-06 06:00:13 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,10 @@
+// Default export: function
+export default function greet() {
+  return "Hello from default export!";
+}
+
+// Named export: object
+export const info = {
+  name: "Maffia",
+  age: 20
+};
