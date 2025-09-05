c5d221b83ac9a21a1128c3e94a3c5a8833973f4b
maffia <maffiakhateg@gmail.com>
2025-09-06 05:53:53 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,15 @@
+// Traditional -> Arrow
+
+// With one parameter
+const greet = (name) => "Hello, " + name;
+
+// With one parameter (implicit return)
+const square = n => n * n;
+
+// No parameters
+const sayHi = () => console.log("Hi!");
+
+// Testing
+console.log(greet("Maffia"));
+console.log(square(5));
+sayHi();
